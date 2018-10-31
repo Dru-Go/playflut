4f8d91d875e622d7e6cfe7fbff86004113da19c7
Dru-Go <drumac2@gmail.com>
2018-10-31 03:50:27 -0700

    Initail commit

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,6 @@ class Textclass extends StatelessWidget {
 }
 
 class MyAppBar extends StatelessWidget {
-
   MyAppBar({this.title});
   final Widget title;
 
@@ -62,14 +61,24 @@ class MyApp extends StatelessWidget {
               style: Theme.of(context).primaryTextTheme.title,
             ),
           ),
-          new Card(
-            child: new Center(
-              child: new Image.asset(
-                'images/project.png',
-                height: 240.0,
-                fit: BoxFit.cover,
+          new Stack(
+            alignment: const Alignment(0.6, 0.6),
+            children: <Widget>[
+              new Image.asset('images/woman.jpg'),
+              new Container(
+                // padding: const EdgeInsets.symmetric(vertical: 200.0),
+
+                margin: EdgeInsetsDirectional.fromSTEB(60.0, 0.0, 150.0, 470.0),
+                child: new Text(
+                  'Arctic',
+                  style: new TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white24,
+                  ),
+                ),
               ),
-            ),
+            ],
           ),
         ],
       ),
@@ -77,11 +86,8 @@ class MyApp extends StatelessWidget {
   }
 }
 
-
-
-
-class App2 extends StatelessWidget{
-  Widget build(BuildContext context){
+class App2 extends StatelessWidget {
+  Widget build(BuildContext context) {
     return new MaterialApp(
       title: 'Wee',
       home: new Scaffold(
@@ -89,7 +95,7 @@ class App2 extends StatelessWidget{
           leading: new IconButton(
             icon: new Icon(Icons.menu),
             tooltip: 'menu',
-            onPressed:null,
+            onPressed: null,
           ),
           title: new Text('Hello Flutter'),
           actions: <Widget>[
@@ -100,121 +106,67 @@ class App2 extends StatelessWidget{
             )
           ],
         ),
+        body: new Stack(
+          alignment: const Alignment(0.6, 0.6),
+          children: <Widget>[
+            new Image.asset('images/woman.jpg'),
+            new Container(
+              margin: EdgeInsetsDirectional.fromSTEB(60.0, 0.0, 150.0, 470.0),
+              child: new Text(
+                'Arctic',
+                style: new TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white30,
+                ),
+              ),
+            ),
+            new Container(
+                margin: EdgeInsetsDirectional.fromSTEB(60.0, 0.0, 150.0, 470.0),
+                child: new Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                      new Icon(Icons.call),
+                      new Container(
+                        child: new Text(
+                          'Call',
+                          style: new TextStyle(
+                              fontSize: 16.0,
+                              fontWeight: FontWeight.w400,
+                              color: Colors.white),
+                        ),
+                      ),
+                  ],
+                )),
+          ],
+        ),
       ),
     );
   }
 }
 
-
-
-
-
-// void main() => runApp(new MyApp());
-
-// class MyApp extends StatelessWidget {
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return new MaterialApp(
-//       title: 'Flutter Demo',
-//       theme: new ThemeData(
-//         // This is the theme of your application.
-//         //
-//         // Try running your application with "flutter run". You'll see the
-//         // application has a blue toolbar. Then, without quitting the app, try
-//         // changing the primarySwatch below to Colors.green and then invoke
-//         // "hot reload" (press "r" in the console where you ran "flutter run",
-//         // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-//         // counter didn't reset back to zero; the application is not restarted.
-//         primarySwatch: Colors.lightGreen
-//       ),
-//       home: new MyHomePage(title: 'My Page'),
-//     );
-//   }
-// }
-
-// class MyHomePage extends StatefulWidget {
-//   MyHomePage({Key key, this.title}) : super(key: key);
-
-//   // This widget is the home page of your application. It is stateful, meaning
-//   // that it has a State object (defined below) that contains fields that affect
-//   // how it looks.
-
-//   // This class is the configuration for the state. It holds the values (in this
-//   // case the title) provided by the parent (in this case the App widget) and
-//   // used by the build method of the State. Fields in a Widget subclass are
-//   // always marked "final".
-
-//   final String title;
-
-//   @override
-//   _MyHomePageState createState() => new _MyHomePageState();
-// }
-
-// class _MyHomePageState extends State<MyHomePage> {
-//   int _counter = 0;
-
-//   void _incrementCounter() {
-//     setState(() {
-//       // This call to setState tells the Flutter framework that something has
-//       // changed in this State, which causes it to rerun the build method below
-//       // so that the display can reflect the updated values. If we changed
-//       // _counter without calling setState(), then the build method would not be
-//       // called again, and so nothing would appear to happen.
-//       _counter++;
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     // This method is rerun every time setState is called, for instance as done
-//     // by the _incrementCounter method above.
-//     //
-//     // The Flutter framework has been optimized to make rerunning build methods
-//     // fast, so that you can just rebuild anything that needs updating rather
-//     // than having to individually change instances of widgets.
-//     return new Scaffold(
-//       appBar: new AppBar(
-//         // Here we take the value from the MyHomePage object that was created by
-//         // the App.build method, and use it to set our appbar title.
-//         title: new Text(widget.title),
-//       ),
-//       body: new Center(
-//         // Center is a layout widget. It takes a single child and positions it
-//         // in the middle of the parent.
-//         child: new Column(
-//           // Column is also layout widget. It takes a list of children and
-//           // arranges them vertically. By default, it sizes itself to fit its
-//           // children horizontally, and tries to be as tall as its parent.
-//           //
-//           // Invoke "debug paint" (press "p" in the console where you ran
-//           // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
-//           // window in IntelliJ) to see the wireframe for each widget.
-//           //
-//           // Column has various properties to control how it sizes itself and
-//           // how it positions its children. Here we use mainAxisAlignment to
-//           // center the children vertically; the main axis here is the vertical
-//           // axis because Columns are vertical (the cross axis would be
-//           // horizontal).
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             new Text(
-//               'You have pushed the button this n times:',
-//             ),
-//             new Text(
-//               '$_counter',
-//               style: Theme.of(context).textTheme.display1,
-//             ),
-//           ],
-//         ),
-//       ),
-//       floatingActionButton: new FloatingActionButton(
-//         onPressed: _incrementCounter,
-//         tooltip: 'Increment',
-//         child: new Icon(Icons.add),
-//       ), // This trailing comma makes auto-formatting nicer for build methods.
-//     );
-//   }
-// }
-
-// class homebar extends
+class fellowcolls extends StatelessWidget {
+  Widget build(BuildContext context) {
+    Column buildButtonColumn(IconData icon, String label) {
+      Color color = Theme.of(context).primaryColor;
+      return new Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          new Icon(icon, color: color),
+          new Container(
+            margin: const EdgeInsets.only(top: 8.0),
+            child: new Text(
+              label,
+              style: new TextStyle(
+                fontSize: 12.0,
+                fontWeight: FontWeight.w400,
+                color: color,
+              ),
+            ),
+          ),
+        ],
+      );
+    }
+  }
+}
